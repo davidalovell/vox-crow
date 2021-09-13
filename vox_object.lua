7666a5f1bdc31b9f86284eb9efed6aa092f2fc86
davidalovell <davidlovell@live.co.uk>
2021-09-13 09:57:05 +0100

    added mask, and wrap

--- vox_object.lua
+++ vox_object.lua
@@ -56,7 +56,7 @@ cv = {
 
 
 -- Vox object
--- DL, last modified 2021-09-12
+-- DL, last modified 2021-09-13
 
 Vox = {}
 function Vox:new(args)
@@ -70,8 +70,11 @@ function Vox:new(args)
   o.transpose, o._transpose = args.transpose == nil and 0 or args.transpose, 0
 
   o.scale = args.scale == nil and cv.scale or args.scale
+  o.mask = args.mask
+  o.wrap = args.wrap == nil and false or args.wrap
   o.negharm = args.negharm == nil and false or args.negharm
-  o.synth = args.synth == nil and function(note, level) ii.jf.play_note(note / 12, level) end or args.synth
+
+  o.synth = args.synth == nil and function(note, level) --[[ii.jf.play_note(note / 12, level)]] return note, level end or args.synth
 
   o.seq = args.seq == nil and {} or args.seq
   o.preset = args.preset == nil and {} or args.preset
@@ -89,7 +92,10 @@ function Vox:play(args)
   self._transpose = args.transpose == nil and self._transpose or args.transpose
 
   self.scale = args.scale == nil and self.scale or args.scale
+  self.mask = args.mask
+  self.wrap = args.wrap == nil and self.wrap or args.wrap
   self.negharm = args.negharm == nil and self.negharm or args.negharm
+
   self.synth = args.synth == nil and self.synth or args.synth
 
   return self:__on() and self.synth(self:__note(), self:__level())
@@ -97,10 +103,13 @@ end
 
 function Vox:__on() return self.on and self._on end
 function Vox:__level() return self.level * self._level end
-function Vox:__octave() return self.octave + self._octave + math.floor(self:__degree() / #self.scale) end
+function Vox:__wrap() return self.wrap and 0 or math.floor(self:__degree() / #self.scale) end
+function Vox:__octave() return self.octave + self._octave + self:__wrap() end
 function Vox:__degree() return (self.degree - 1) + (self._degree - 1) end
 function Vox:__transpose() return self.transpose + self._transpose end
-function Vox:__pos() return self.scale[self:__degree() % #self.scale + 1] + self:__transpose() end
+function Vox:__val() return self.scale[self:__degree() % #self.scale + 1] end
+function Vox:__mask() return self.mask == nil and self:__val() or self.scale[selector(self:__val(), self.mask, 1, #self.scale)] end
+function Vox:__pos() return self:__mask() + self:__transpose() end
 function Vox:__neg() return (7 - self:__pos()) % 12 end
 function Vox:__note() return (self.negharm and self:__neg() or self:__pos()) + self:__octave() * 12 end
 
@@ -117,3 +126,25 @@ function _do(objects, method, args)
   end
 end
 --
+
+
+
+
+
+function clamp(x, min, max)
+  return math.min( math.max( min, x ), max )
+end
+
+function round(x)
+  return x % 1 >= 0.5 and math.ceil(x) or math.floor(x)
+end
+
+function linlin(x, in_min, in_max, out_min, out_max)
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+end
+
+function selector(x, data, in_min, in_max, out_min, out_max)
+  out_min = out_min or 1
+  out_max = out_max or #data
+  return data[ clamp( round( linlin( x, in_min, in_max, out_min, out_max ) ), out_min, out_max ) ]
+end
