bd879a1b54d52e8a2c9788e5e5429c53100dcbad
davidalovell <davidlovell@live.co.uk>
2021-09-17 08:00:14 +0100

    debug begins

--- vox_object.lua
+++ vox_object.lua
@@ -132,19 +132,28 @@ function Vox:play(args)
     local closest_val = args.mask[1]
     local lowest_val = args.mask[1] --
 
+    print('')
     for _, val in ipairs(args.mask) do
+      print('key', _, 'val', val)
       val = (val - 1) % #args.scale + 1
+      print('val', val)
       closest_val = math.abs(val - args.ix) < math.abs(closest_val - args.ix) and val or closest_val
+      print('closest_val', closest_val)
       lowest_val = val < lowest_val and val or lowest_val
+      print('lowest_val', lowest_val)
     end
 
     local highest_val = lowest_val + #args.scale --
+    print('highest_val', highest_val)
     closest_val = math.abs(highest_val - args.ix) < math.abs(closest_val - args.ix) --
       and highest_val
       or closest_val
+    print('closest_val', closest_val)
 
     args.ix = (closest_val - 1) % #args.scale + 1
+    print('args.ix', args.ix)
     args.octave = args.octave + math.floor(closest_val / #args.scale)
+    print('args.octave', args.octave)
   end
 
 
