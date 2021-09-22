950d0c06fe02a69be372a659b7f002b4eeb200d1
davidalovell <50540302+davidalovell@users.noreply.github.com>
2021-09-22 20:47:45 +0100

    Update vox_object.lua

--- vox_object.lua
+++ vox_object.lua
@@ -127,10 +127,9 @@ function Vdo(objects, method, args)
 end
 
 function Vdyn(data)
-  local updated_data = {}
+  local updated = {}
   for k, v in pairs(data) do
-    -- updated_data[k] = data[k]()
-    updated_data[k] = type(v) == 'function' and data[k]() or data[k]
+    updated[k] = type(v) == 'function' and data[k]() or data[k]
   end
-  return updated_data
+  return updated
 end
